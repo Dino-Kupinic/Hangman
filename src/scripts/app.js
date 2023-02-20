546d78eeb88c640a3d247b4351f60883101488bc
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-02-20 18:41:46 +0100

    added async run()

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,17 @@
 "use strict";
 
+import jsonLoader from "./fileLoader.js";
+
+window.onload = () => {
+	run().then(() => {});
+}
+
+async function run() {
+	try {
+		const json = await jsonLoader("animals");
+		console.log(json);
+
+	} catch (error) {
+		console.log(error);
+	}
+}
