b80e1b5f8952a19db8d39d42b4389b8b592ae4a7
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-02-23 13:45:31 +0100

    added display of hangman

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,39 +12,9 @@ function run() {
 	addEventListenersToKeys();
 }
 
-function addEventListenersToKeys() {
-	const keys = document.querySelectorAll(".key");
-	keys.forEach(key => {
-		key.addEventListener("click", () => {
-			const letter = key.querySelector(".letter");
-			const value = letter.textContent.toLowerCase();
-
-			console.log(value)
-			console.log(wordArray)
-			console.log(displayArray)
-
-			if (wordArray.includes(value)) {
-				for (let i = 0; i < wordArray.length; i++) {
-					if (wordArray[i] === value) {
-						displayArray[i] = value;
-					}
-				}
-
-				updateWordDisplay(displayArray)
-
-				if (wordArray.toString() == displayArray.toString()) {
-					console.log("win")
-				}
-			} else {
-				console.log("hangman")
-			}
-		})
-	})
-}
 
 const displayArray = [];
 let wordArray;
-
 
 function clearWord() {
 	const htmlWord = document.querySelector("#word");
@@ -53,12 +23,12 @@ function clearWord() {
 }
 
 function updateWordDisplay(displayArray) {
-	const NO_BREAK_SPACE = "\u00A0";
+	const UTF_NO_BREAK_SPACE = "\u00A0";
 	const ALL_SPACE_GLOBAL = / /g;
 
 	const htmlWord = clearWord();
 	const joinedString = displayArray.join(" ");
-	htmlWord.textContent = joinedString.replace(ALL_SPACE_GLOBAL, NO_BREAK_SPACE);
+	htmlWord.textContent = joinedString.replace(ALL_SPACE_GLOBAL, UTF_NO_BREAK_SPACE);
 }
 
 function displayWord(response) {
@@ -91,4 +61,54 @@ function chooseRandomWord(response) {
 
 function getRandomNumber(length) {
 	return Math.floor(Math.random() * (length - 1));
+}
+
+function addEventListenersToKeys() {
+	let incorrectGuesses = 0;
+	const keys = document.querySelectorAll(".key");
+	keys.forEach(key => {
+		key.addEventListener("click", () => {
+			const letter = key.querySelector(".letter");
+			const value = letter.textContent.toLowerCase();
+
+			console.log(value)
+			console.log(wordArray)
+			console.log(displayArray)
+
+			if (wordArray.includes(value)) {
+				for (let i = 0; i < wordArray.length; i++) {
+					if (wordArray[i] === value) {
+						displayArray[i] = value;
+					}
+				}
+
+				updateWordDisplay(displayArray)
+
+				if (wordArray.toString() === displayArray.toString()) {
+					console.log("win")
+				}
+			} else {
+				incorrectGuesses++;
+			}
+			updateHangman(incorrectGuesses);
+		});
+	});
+}
+
+function updateHangman(incorrectGuesses) {
+	const hangmanParts = [
+		'  _________\n  |        |\n  |\n  |\n  |\n  |\n _|\n|_|______',
+		'  _________\n  |        |\n  |        O\n  |\n  |\n  |\n _|\n|_|______',
+		'  _________\n  |        |\n  |        O\n  |        |\n  |        |\n  |\n _|\n|_|______',
+		'  _________\n  |        |\n  |        O\n  |       /|\n  |        |\n  |\n _|\n|_|______',
+		'  _________\n  |        |\n  |        O\n  |       /|\\\n  |        |\n  |\n _|\n|_|______',
+		'  _________\n  |        |\n  |        O\n  |       /|\\\n  |        |\n  |       / \\\n _|\n|_|______'
+	];
+
+	const hangman = document.querySelector("pre");
+	if (incorrectGuesses <= 5) {
+		hangman.innerText = hangmanParts[incorrectGuesses];
+	} else {
+		// lose
+	}
 }
